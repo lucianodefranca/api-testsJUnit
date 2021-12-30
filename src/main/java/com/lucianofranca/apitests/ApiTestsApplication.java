4157b23b2719fa511859091eba6ca08c16323f69
Luciano de Franca <lucianodefranca86@gmail.com>
2021-12-29 23:08:10 -0300

    Projeto iniciado

---
+++ src/main/java/com/lucianofranca/apitests/ApiTestsApplication.java
@@ -0,0 +1,13 @@
+package com.lucianofranca.apitests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucianofranca/apitests/ApiTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianofranca.apitests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
