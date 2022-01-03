acbff1cb9bd34d4dc6ef3042e06c522c0b81014c
Luciano de Franca <lucianodefranca86@gmail.com>
2022-01-03 17:38:42 -0300

    Classe UserResource findById

--- src/main/java/com/lucianofranca/apitests/resource/UserResource.java
+++ src/main/java/com/lucianofranca/apitests/resource/UserResource.java
@@ -1,6 +1,8 @@
 package com.lucianofranca.apitests.resource;
 
 import com.lucianofranca.apitests.domain.User;
+import com.lucianofranca.apitests.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(new User(1, "Luciano", "luciano@gmail.com", "1234"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
