b18d8e88c111231939060212afe17c864b503742
Luciano de Franca <lucianodefranca86@gmail.com>
2022-01-03 18:18:18 -0300

    Tratamento de excecao para objeto nao encontrado

--- src/main/java/com/lucianofranca/apitests/services/UserServiceImpl.java
+++ src/main/java/com/lucianofranca/apitests/services/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.lucianofranca.apitests.services;
 
 import com.lucianofranca.apitests.domain.User;
 import com.lucianofranca.apitests.rrepositories.UserRepository;
+import com.lucianofranca.apitests.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,6 @@ public class UserServiceImpl implements UserService{
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
     }
 }
