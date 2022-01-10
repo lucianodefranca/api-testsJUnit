7b46521cc3623d9a15691ee6fb01f4d7005366f0
Luciano de Franca <lucianodefranca86@gmail.com>
2022-01-09 22:20:40 -0300

    teste para classe user service metodo create com sucesso

--- src/test/java/com/lucianofranca/apitests/services/UserServiceImplTest.java
+++ src/test/java/com/lucianofranca/apitests/services/UserServiceImplTest.java
@@ -18,6 +18,7 @@ import java.util.Optional;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
 class UserServiceImplTest {
@@ -89,7 +90,17 @@ class UserServiceImplTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSuccess() {
+        Mockito.when(repository.save(any())).thenReturn(user);
+
+        User response = service.create(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
